61d295ca6d789377f578a48fc9e37a73163b1203
ice breaker <1324318532@qq.com>
2024-09-05 09:16:26 +0800

    chore: bump version

--- packages/postcss-rem-to-viewport/tsup.config.ts
+++ packages/postcss-rem-to-viewport/tsup.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   clean: true,
   dts: true,
+  outExtension(ctx) {
+    return {
+      js: `.${ctx.format === 'cjs' ? 'cjs' : 'mjs'}`,
+    }
+  },
   // https://github.com/egoist/tsup/pull/1056
   // https://github.com/egoist/tsup/issues?q=cjsInterop
   // cjsInterop: true,
